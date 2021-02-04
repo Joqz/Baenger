4a346e9e79a6cc566153445299f2251d629f0aa4
Joel Ala-aho <43337796+Joqz@users.noreply.github.com>
2021-02-04 14:56:43 +0200

    Created "logged in" page and the ability to create playlists through our app

---
+++ app/src/main/java/com/example/baenger/LoggedInActivity.kt
@@ -0,0 +1,69 @@
+package com.example.baenger
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.spotify.sdk.android.authentication.AuthenticationClient
+import com.spotify.sdk.android.authentication.AuthenticationResponse
+import kotlinx.android.synthetic.main.activity_loggedin.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import java.io.OutputStream
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
+
+
+class LoggedInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val spotifyId = intent.getStringExtra("spotify_id")
+        val spotifyDisplayName = intent.getStringExtra("spotify_display_name")
+        val spotifyEmail = intent.getStringExtra("spotify_email")
+        val spotifyAvatarURL = intent.getStringExtra("spotify_avatar")
+        val spotifyAccessToken = intent.getStringExtra("spotify_access_token")
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loggedin)
+
+        spotifyname_textview.text = spotifyDisplayName
+
+        spotify_create_playlist.setOnClickListener {createPlaylist(spotifyAccessToken, spotifyId)}
+    }
+
+    private fun createPlaylist(spotifyAccessToken: String?, spotifyId: String?) {
+        Log.d("Status: ", "Please Wait...")
+        if (spotifyAccessToken == null) {
+            Log.i("Status: ", "Something went wrong - No Access Token found")
+            return
+        }
+
+        val getplaylistURL = "https://api.spotify.com/v1/users/" + spotifyId + "/playlists"
+
+        GlobalScope.launch(Dispatchers.Default) {
+            val url = URL(getplaylistURL)
+            val httpsURLConnection = withContext(Dispatchers.IO) {url.openConnection() as HttpsURLConnection }
+            httpsURLConnection.requestMethod = "POST"
+            httpsURLConnection.setRequestProperty("Authorization", "Bearer $spotifyAccessToken")
+            httpsURLConnection.setRequestProperty("Accept", "application/json")
+            httpsURLConnection.setRequestProperty("Content-Type", "application/json")
+            httpsURLConnection.doInput = true
+            httpsURLConnection.doOutput = true
+
+            val requestJson = JSONObject("""{"name":"Baenger", "description":"Playlist for Baenger","public":"false"}""")
+
+            val os: OutputStream = httpsURLConnection.getOutputStream()
+            os.write(requestJson.toString().toByteArray())
+            os.close()
+
+            val response = httpsURLConnection.inputStream.bufferedReader()
+                .use { it.readText() }  // defaults to UTF-8
+            withContext(Dispatchers.Main) {
+                val jsonObject = JSONObject(response)
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/baenger/MainActivity.kt
+++ app/src/main/java/com/example/baenger/MainActivity.kt
@@ -4,10 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
 import com.spotify.sdk.android.authentication.AuthenticationClient
 import com.spotify.sdk.android.authentication.AuthenticationRequest
 import com.spotify.sdk.android.authentication.AuthenticationResponse
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -17,18 +17,23 @@ import java.net.URL
 import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
+
+    var id = ""
+    var displayName = ""
+    var email = ""
+    var avatar = ""
+    var accessToken = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val spotify_login_btn = findViewById(R.id.spotify_login_btn) as Button
-
         spotify_login_btn.setOnClickListener {
             val request = getAuthenticationRequest(AuthenticationResponse.Type.TOKEN)
             AuthenticationClient.openLoginActivity(
-                    this,
-                    SpotifyConstants.AUTH_TOKEN_REQUEST_CODE,
-                    request
+                this,
+                SpotifyConstants.AUTH_TOKEN_REQUEST_CODE,
+                request
             )
         }
 
@@ -36,9 +41,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun getAuthenticationRequest(type: AuthenticationResponse.Type): AuthenticationRequest {
         return AuthenticationRequest.Builder(SpotifyConstants.CLIENT_ID, type, SpotifyConstants.REDIRECT_URI)
-                .setShowDialog(false)
-                .setScopes(arrayOf("user-read-email"))
-                .build()
+            .setShowDialog(false)
+            .setScopes(arrayOf("user-read-email", "playlist-modify-public", "playlist-modify-private"))
+            .build()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -49,7 +54,6 @@ class MainActivity : AppCompatActivity() {
             fetchSpotifyUserProfile(accessToken)
         }
     }
-
 
     private fun fetchSpotifyUserProfile(token: String?) {
         Log.d("Status: ", "Please Wait...")
@@ -62,27 +66,30 @@ class MainActivity : AppCompatActivity() {
 
         GlobalScope.launch(Dispatchers.Default) {
             val url = URL(getUserProfileURL)
-            val httpsURLConnection = withContext(Dispatchers.IO) {url.openConnection() as HttpsURLConnection }
+            val httpsURLConnection = withContext(Dispatchers.IO) {url.openConnection() as HttpsURLConnection}
             httpsURLConnection.requestMethod = "GET"
             httpsURLConnection.setRequestProperty("Authorization", "Bearer $token")
             httpsURLConnection.doInput = true
             httpsURLConnection.doOutput = false
             val response = httpsURLConnection.inputStream.bufferedReader()
-                    .use { it.readText() }  // defaults to UTF-8
+                .use { it.readText() }  // defaults to UTF-8
             withContext(Dispatchers.Main) {
                 val jsonObject = JSONObject(response)
 
                 // Spotify Id
                 val spotifyId = jsonObject.getString("id")
                 Log.d("Spotify Id :", spotifyId)
+                id = spotifyId
 
                 // Spotify Display Name
                 val spotifyDisplayName = jsonObject.getString("display_name")
                 Log.d("Spotify Display Name :", spotifyDisplayName)
+                displayName = spotifyDisplayName
 
                 // Spotify Email
                 val spotifyEmail = jsonObject.getString("email")
                 Log.d("Spotify Email :", spotifyEmail)
+                email = spotifyEmail
 
 
                 val spotifyAvatarArray = jsonObject.getJSONArray("images")
@@ -91,12 +98,25 @@ class MainActivity : AppCompatActivity() {
                 if (spotifyAvatarArray.length() > 0) {
                     spotifyAvatarURL = spotifyAvatarArray.getJSONObject(0).getString("url")
                     Log.d("Spotify Avatar : ", spotifyAvatarURL)
+                    avatar = spotifyAvatarURL
                 }
 
                 Log.d("Spotify AccessToken :", token)
+                accessToken = token
 
+                openLoggedInActivity()
             }
         }
+    }
+
+    private fun openLoggedInActivity() {
+        val myIntent = Intent(this@MainActivity, LoggedInActivity::class.java)
+        myIntent.putExtra("spotify_id", id)
+        myIntent.putExtra("spotify_display_name", displayName)
+        myIntent.putExtra("spotify_email", email)
+        myIntent.putExtra("spotify_avatar", avatar)
+        myIntent.putExtra("spotify_access_token", accessToken)
+        startActivity(myIntent)
     }
 
 
