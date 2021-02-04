adc9476135d6ca8cd6da925ecfe795622f8bb863
Joel Ala-aho <43337796+Joqz@users.noreply.github.com>
2021-02-04 12:00:06 +0200

    Added simple spotify integration

--- app/src/main/java/com/example/baenger/MainActivity.kt
+++ app/src/main/java/com/example/baenger/MainActivity.kt
@@ -1,11 +1,103 @@
 package com.example.baenger
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.spotify.sdk.android.authentication.AuthenticationClient
+import com.spotify.sdk.android.authentication.AuthenticationRequest
+import com.spotify.sdk.android.authentication.AuthenticationResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spotify_login_btn = findViewById(R.id.spotify_login_btn) as Button
+
+        spotify_login_btn.setOnClickListener {
+            val request = getAuthenticationRequest(AuthenticationResponse.Type.TOKEN)
+            AuthenticationClient.openLoginActivity(
+                    this,
+                    SpotifyConstants.AUTH_TOKEN_REQUEST_CODE,
+                    request
+            )
+        }
+
     }
+
+    private fun getAuthenticationRequest(type: AuthenticationResponse.Type): AuthenticationRequest {
+        return AuthenticationRequest.Builder(SpotifyConstants.CLIENT_ID, type, SpotifyConstants.REDIRECT_URI)
+                .setShowDialog(false)
+                .setScopes(arrayOf("user-read-email"))
+                .build()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (SpotifyConstants.AUTH_TOKEN_REQUEST_CODE == requestCode) {
+            val response = AuthenticationClient.getResponse(resultCode, data)
+            val accessToken: String? = response.accessToken
+            fetchSpotifyUserProfile(accessToken)
+        }
+    }
+
+
+    private fun fetchSpotifyUserProfile(token: String?) {
+        Log.d("Status: ", "Please Wait...")
+        if (token == null) {
+            Log.i("Status: ", "Something went wrong - No Access Token found")
+            return
+        }
+
+        val getUserProfileURL = "https://api.spotify.com/v1/me"
+
+        GlobalScope.launch(Dispatchers.Default) {
+            val url = URL(getUserProfileURL)
+            val httpsURLConnection = withContext(Dispatchers.IO) {url.openConnection() as HttpsURLConnection }
+            httpsURLConnection.requestMethod = "GET"
+            httpsURLConnection.setRequestProperty("Authorization", "Bearer $token")
+            httpsURLConnection.doInput = true
+            httpsURLConnection.doOutput = false
+            val response = httpsURLConnection.inputStream.bufferedReader()
+                    .use { it.readText() }  // defaults to UTF-8
+            withContext(Dispatchers.Main) {
+                val jsonObject = JSONObject(response)
+
+                // Spotify Id
+                val spotifyId = jsonObject.getString("id")
+                Log.d("Spotify Id :", spotifyId)
+
+                // Spotify Display Name
+                val spotifyDisplayName = jsonObject.getString("display_name")
+                Log.d("Spotify Display Name :", spotifyDisplayName)
+
+                // Spotify Email
+                val spotifyEmail = jsonObject.getString("email")
+                Log.d("Spotify Email :", spotifyEmail)
+
+
+                val spotifyAvatarArray = jsonObject.getJSONArray("images")
+                //Check if user has Avatar
+                var spotifyAvatarURL = ""
+                if (spotifyAvatarArray.length() > 0) {
+                    spotifyAvatarURL = spotifyAvatarArray.getJSONObject(0).getString("url")
+                    Log.d("Spotify Avatar : ", spotifyAvatarURL)
+                }
+
+                Log.d("Spotify AccessToken :", token)
+
+            }
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/example/baenger/SpotifyConstants.kt
@@ -0,0 +1,7 @@
+package com.example.baenger
+
+object SpotifyConstants {
+    val CLIENT_ID = "748566c3bf514d918b38457e48c23045"
+    val AUTH_TOKEN_REQUEST_CODE = 0x10
+    val REDIRECT_URI = "baenger://"
+}
