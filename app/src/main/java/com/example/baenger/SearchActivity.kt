3daf3d659152746d6b2a224e313da6a1fb535f62
Ossi <karja.ossi@gmail.com>
2021-02-10 23:06:00 +0200

    Activity for search and some small UI stuff added.

---
+++ app/src/main/java/com/example/baenger/SearchActivity.kt
@@ -0,0 +1,33 @@
+package com.example.baenger
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import com.spotify.sdk.android.authentication.AuthenticationClient
+import com.spotify.sdk.android.authentication.AuthenticationRequest
+import com.spotify.sdk.android.authentication.AuthenticationResponse
+import kotlinx.android.synthetic.main.activity_loggedin.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
+
+class SearchActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+        supportActionBar?.hide()
+
+
+        spotifyname_textview.text = spotifyDisplayName
+    }
+
+
+}
